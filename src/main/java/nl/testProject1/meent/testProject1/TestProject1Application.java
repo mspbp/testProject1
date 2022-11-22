b3af2ff712d6acf509730b831a376d0d4031e649
mspbp <63860410+mspbp@users.noreply.github.com>
2022-11-22 12:38:27 +0100

    hoi

---
+++ src/main/java/nl/testProject1/meent/testProject1/TestProject1Application.java
@@ -0,0 +1,13 @@
+package nl.testProject1.meent.testProject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProject1Application.class, args);
+	}
+
+}
